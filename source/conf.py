9e95f9473bb402be77bde0be5bf06f0fa4ffc13e
TaiYeong <runabird27@gmail.com>
2023-12-01 18:47:53 +0900

    init

--- source/conf.py
+++ source/conf.py
@@ -17,12 +17,12 @@ release = '1.0'
 extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'  # in conf.py file
+html_theme = 'sphinx_rtd_theme' 
 html_static_path = ['_static']
--- source/index.rst
+++ source/index.rst
@@ -1,5 +1,5 @@
 .. ssongDocs documentation master file, created by
-   sphinx-quickstart on Fri Dec  1 17:32:51 2023.
+   sphinx-quickstart on Fri Dec  1 18:46:21 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
